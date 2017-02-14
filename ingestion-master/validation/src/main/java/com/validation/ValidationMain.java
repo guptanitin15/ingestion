c3db68c2a3e3bc870ea6e37f5c9c8b8eb965ab52
Nitin Gupta <gupta.nitin015@gmail.com>
2017-02-14 21:07:48 +0530

    first commit :setup maven modules

---
+++ ingestion-master/validation/src/main/java/com/validation/ValidationMain.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package com.validation;
+
+/**
+ * @author nitin
+ *
+ */
+public class ValidationMain {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("hello world!");
+		
+	}
+
+}
